165a411ff9da2a0a8d21d4006f080816a921dc47
Saberdream <Saberdream@users.noreply.github.com>
2021-12-26 14:00:32 +0100

    Version 1.0.2

---
+++ antibbr.meta.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name        JV-AntiBBR
+// @namespace   JV-AntiBBR
+// @description Efface les messages contenant les liens BBR des forums.
+// @include     http*://www.jeuxvideo.com/forums/42-*
+// @include     http*://www.jeuxvideo.com/forums/0-*
+// @version     1.0.2
+// @require     http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// @updateURL   https://raw.githubusercontent.com/Saberdream/JV-AntiBBR/main/JV-AntiBBR.meta.js
+// @downloadURL https://raw.githubusercontent.com/Saberdream/JV-AntiBBR/main/JV-AntiBBR.user.js
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @grant       GM_listValues
+// @grant       GM_deleteValue
+// @grant       GM_addStyle
+// @icon        https://i.imgur.com/fRWc0Lr.png
+// @author      Jyren
+// @noframes
+// ==/UserScript==
